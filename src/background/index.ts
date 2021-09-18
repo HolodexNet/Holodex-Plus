3d1819daa14f9adbd992de0d1972d9977fb62e94
jprochazk <honza.spacir@gmail.com>
2021-09-18 21:25:52 +0200

    re-add yt header filtering

--- src/background/index.ts
+++ src/background/index.ts
@@ -9,3 +9,16 @@ browser.runtime.onMessage.addListener((message, sender) => {
   if (!tabId) return;
   browser.tabs.sendMessage(tabId, message);
 });
+
+// Allows all of youtube to be iframed (mainly used for Archive Chat)
+browser.webRequest.onHeadersReceived.addListener(
+  function (details) {
+    return {
+      responseHeaders: details?.responseHeaders?.filter((header) => header.name.toLowerCase() !== "x-frame-options"),
+    };
+  },
+  {
+    urls: ["*://*.youtube.com/*"],
+  },
+  ["blocking", "responseHeaders"]
+);
