f337706ef009354a597e01514f368155f09dad58
jprochazk <honza.spacir@gmail.com>
2021-09-19 10:16:05 +0200

    remove log overrides

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,8 +1,3 @@
-(["info", "warn", "error", "debug"] as const).forEach((fn) => {
-  const orig = console[fn].bind(console);
-  console[fn] = (...args: any[]) => orig("[Holodex+]", ...args);
-});
-
 export function inject(scriptPath: string) {
   const el = document.createElement("script");
   el.src = scriptPath;
