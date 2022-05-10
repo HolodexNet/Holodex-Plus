76d47f7cc3e097f4f5298082c76faa9c91151c14
MonMonMnemonics <mikatzukiathanas@gmail.com>
2022-05-10 16:43:49 +0900

    permission and content scripts change

--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -6,16 +6,16 @@ const content_scripts = [
   { matches: ["*://*.holodex.net/*"], js: ["content/holodex.js"], run_at: "document_end" },
   { matches: ["*://*.youtube.com/embed/*"], js: ["content/yt-player.js"], all_frames: true, run_at: "document_end" },
   { matches: ["*://*.youtube.com/live_chat*"], js: ["content/yt-chat.js", "content/yt-chat-tlsync.js"], all_frames: true, run_at: "document_end" },
-  { matches: ["*://*.twitch.tv/embed/*/chat?*"], js: ["content/twitch-chat-tlsync.js"], all_frames: true, run_at: "document_end" },
+  //{ matches: ["*://*.twitch.tv/embed/*/chat?*"], js: ["content/twitch-chat-tlsync.js"], all_frames: true, run_at: "document_end" },
   {
     matches: ["*://*.youtube.com/*"],
     run_at: "document_start",
     js: ["content/yt-watch.js"],
     css: ["content/style/yt-watch.css"],
   },
-  { matches: ["*://*.twitcasting.tv/*/embeddedplayer/*"], js: ["content/twitcast-embed-player.js"], all_frames: true, run_at: "document_end" },
-  { matches: ["*://embed.nicovideo.jp/watch/*"], js: ["content/niconico-embed-player.js"], all_frames: true, run_at: "document_end" },
-  { matches: ["*://player.bilibili.com/player.html*"], js: ["content/bilibili-embed-player.js"], all_frames: true, run_at: "document_end" },
+  //{ matches: ["*://*.twitcasting.tv/*/embeddedplayer/*"], js: ["content/twitcast-embed-player.js"], all_frames: true, run_at: "document_end" },
+  //{ matches: ["*://embed.nicovideo.jp/watch/*"], js: ["content/niconico-embed-player.js"], all_frames: true, run_at: "document_end" },
+  //{ matches: ["*://player.bilibili.com/player.html*"], js: ["content/bilibili-embed-player.js"], all_frames: true, run_at: "document_end" },
 ];
 
 const web_accessible_resources = [
@@ -26,7 +26,8 @@ const web_accessible_resources = [
   "content/holodex-flag.inject.js",
 ];
 
-const hosts = ["*://*.youtube.com/*", "*://*.holodex.net/*", "*://*.twitch.tv/*", "*://*.twitcasting.tv/*", "*://embed.nicovideo.jp/*","*://player.bilibili.com/*"];
+//const hosts = ["*://*.youtube.com/*", "*://*.holodex.net/*", "*://*.twitch.tv/*", "*://*.twitcasting.tv/*", "*://embed.nicovideo.jp/*","*://player.bilibili.com/*"];
+const hosts = ["*://*.youtube.com/*", "*://*.holodex.net/*"];
 const permissions = ["tabs", "storage", "webRequest", "webRequestBlocking", ...hosts];
 const name = "Holodex Plus";
 
